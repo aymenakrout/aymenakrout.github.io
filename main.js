e5f4d902fb6e17c4a48511b7bbeab74186d7a7d1
Aymen Akrout <152626399+aymenakrout@users.noreply.github.com>
2024-03-08 14:38:47 +0100

    adding files
    Combining my love for Taekwondo and e-commerce, I've ventured into the world of online retail with a website focused on selling Taekwondo products. From uniforms to training gear, my platform offers a carefully curated selection to meet the needs of practitioners at all levels. With a dedication to quality and customer satisfaction, I aim to provide a seamless shopping experience for Taekwondo enthusiasts worldwide. Join me as I strive to empower practitioners and elevate the sport through my online store.

---
+++ main.js
@@ -0,0 +1,166 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "gloves",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "foot pads",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Gum shiels",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Helmet",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Groin guard",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+
+
+var plusBtns = Array.from(document.querySelectorAll('.fa-circle-plus'));
+var minusBtns = Array.from(document.querySelectorAll('.fa-circle-minus'));
+
+
+
+// Plus btn
+for (let plusBtn of plusBtns) {
+    plusBtn.addEventListener("click", function() {
+        plusBtn.nextElementSibling.innerHTML++
+        total()
+    })
+}
+
+// minus btn
+for (let minusBtn of minusBtns) {
+    minusBtn.addEventListener("click", function() {
+        minusBtn.previousElementSibling.innerHTML > 0 ? minusBtn.previousElementSibling.innerHTML-- : null
+        total()
+    })
+}
+
+
+// Function total price
+function total() {
+    let qte = Array.from(document.querySelectorAll('.counter'))
+    let price = Array.from(document.querySelectorAll('.unit-price'))
+
+    let s = 0
+
+    for (let i = 0; i < price.length; i++) {
+        s = s + price[i].innerHTML * qte[i].innerHTML
+    }
+    document.getElementById('total-price').innerHTML = s
+}
